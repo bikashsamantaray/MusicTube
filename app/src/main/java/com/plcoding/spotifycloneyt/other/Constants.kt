72d040ec85739e4e1e85ac506e8c2a03a1c5eb2d
BIKASH <samantaraybikash2015@gmail.com>
2023-07-01 03:39:41 +0530

    connecting services

--- app/src/main/java/com/plcoding/spotifycloneyt/other/Constants.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/other/Constants.kt
@@ -1,6 +1,8 @@
 package com.plcoding.spotifycloneyt.other
 
-object Constants {
+object  Constants {
 
     const val SONG_COLLECTION = "songs"
+    const val NOTIFICATION_ID = 1
+    const val NOTIFICATION_CHANNEL_ID = "music"
 }
